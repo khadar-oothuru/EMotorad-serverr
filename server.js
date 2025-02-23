d3ed6d1e882c0cf2a9b5a8fe716e887d1d45db22
khadar-oothuru <khadaroothuru@gmail.com>
2025-02-23 13:35:39 +0530

    some changes

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 app.use("/auth", authRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!"); 
+  res.send("EMotorad Server !"); 
 });
 
 
